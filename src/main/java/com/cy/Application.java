bff75b6642c29d15e550fd16bcfd03124fdbe048
51045 <51045@DESKTOP-TQ6JTV0>
2019-09-19 11:59:01 +0800

    按按

--- src/main/java/com/cy/Application.java
+++ src/main/java/com/cy/Application.java
@@ -2,7 +2,7 @@ package com.cy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//12345
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
